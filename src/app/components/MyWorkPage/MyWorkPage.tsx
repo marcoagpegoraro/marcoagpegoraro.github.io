41398a60e8cd0bd8eb819a43308b9933c0b82484
Marco Antonio <marco@Marcos-MacBook-Air.local>
2023-05-26 21:57:13 -0300

    feito com slide o monitor

--- src/app/components/MyWorkPage/MyWorkPage.tsx
+++ src/app/components/MyWorkPage/MyWorkPage.tsx
@@ -1,26 +1,49 @@
 
 import React, { useEffect, useState } from 'react';
-import { Avatar, Card, Paper, Typography } from '@mui/material';
+import { Avatar, Box, Card, Paper, Typography } from '@mui/material';
 import './Styles.css'
 import useWindowDimensions from '../../hooks/useWindowDimensions';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import CloseIcon from '@mui/icons-material/Close';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 export const MyWorkPage = () => {
 
-  const { width, height } = useWindowDimensions();
-
-  const resizeMonitorScreen = () => width < 1034 ? width / 3.2 - 390 : -67.58
+  const images = [
+    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+  ];
 
   return <>
     <div className='my-work-page'>
       <div className='monitor' style={{ backgroundImage: `url('/imac.png')` }}>
-        <div className='monitor-screen' style={{ bottom: resizeMonitorScreen(), backgroundImage: `url('/notes-sem-nada.png')`   }}>
-
-          <div className='app-name'>Meus trabalhos</div>
-          <div className='all-notes'></div>
-          <div className='tasks'></div>
-          <div className='text'></div>
+        <div className='slide'>
+          <div className='titlebar'>
+            <Typography marginLeft={1} color={'white'} fontSize={15} fontFamily={'MS Sans Serif'}>Sobre mim</Typography>
+            <div style={{flex: 1}}></div>
+            <Box width={16} height={16} style={{backgroundColor: '#C2C6CA', marginRight: 2, marginTop: 1}}><HorizontalRuleIcon style={{'fontSize': '1rem', marginTop: 5}}/></Box>
+            <Box width={16} height={16} style={{backgroundColor: '#C2C6CA', marginRight: 2, marginTop: 1}}><CheckBoxOutlineBlankIcon style={{'fontSize': '1rem'}}/></Box>
+            <Box width={16} height={16} style={{backgroundColor: '#C64830', marginRight: 1, marginTop: 1}}><CloseIcon style={{'fontSize': '1rem'}}/></Box>
+          </div>
+          <Carousel>
+            <div>
+              <img src={images[0]} />
+              aaa
+              {/* <p className="legend">AAAAA</p> */}
+            </div>
+            <div>
+              <img src={images[1]} />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src={images[2]} />
+              <p className="legend">Legend 3</p>
+            </div>
+          </Carousel>
         </div>
-
       </div>
     </div>
   </>
