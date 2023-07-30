752ddc55ef0a01b22171cfa6885760ca13850a26
Marco Antonio <marco@Marcos-MacBook-Air.local>
2023-07-29 21:05:12 -0300

    social network buttons componentizado

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,6 +12,7 @@ import { FlexBreakLine, FlexFillRow } from './FlexHelpers';
 import { useTranslation } from 'react-i18next'
 import { Context } from 'react';
 import './NavBar.css'
+import { SocialNetworkButtons } from './SocialNetworkButtons';
 
 interface props {
   colorModeContext: Context<any>
@@ -70,23 +71,7 @@ export const NavBar = ({ colorModeContext }: props) => {
           {toggleDarkModeButton("select-mobile-screen")}
           <FlexBreakLine className='break-line-mobile-screen' />
           <section className='social-network-buttons'>
-
-            <IconButton href="https://www.facebook.com/tete5423" target="_blank" rel="noopener noreferrer" size="large" aria-label="menu" sx={{ ml: 2 }} >
-              <FacebookIcon className='social-network-white-background' sx={{ color: "#3b5998", backgroundSize: '15px 17.3px' }}></FacebookIcon>
-            </IconButton>
-            <IconButton href="https://www.linkedin.com/in/marco-antonio-goncalves/" target="_blank" rel="noopener noreferrer" size="large" aria-label="menu" >
-              <LinkedInIcon className='social-network-white-background' sx={{ color: "#0e76a8" }}></LinkedInIcon>
-            </IconButton>
-            <IconButton href="https://www.instagram.com/marcoantonio.png/" target="_blank" rel="noopener noreferrer" size="large" aria-label="menu" >
-              <InstagramIcon className='social-network-white-background' sx={{ color: "#DD2A7B" }} />
-            </IconButton>
-            <IconButton href="https://medium.com/@tete5423" target="_blank" rel="noopener noreferrer" size="large" aria-label="menu" style={{ color: '#DD2A7B' }}>
-              <img style={{ width: 24, height: 24, padding: 2, borderRadius: 7 }}
-                src='https://miro.medium.com/v2/resize:fill:176:176/1*sHhtYhaCe2Uc3IU0IgKwIQ.png' />
-            </IconButton>
-            <IconButton href="https://github.com/marcoagpegoraro" target="_blank" rel="noopener noreferrer" size="large" aria-label="menu" sx={{ mr: 2, color: 'text.primary' }}>
-              <GitHubIcon />
-            </IconButton>
+            <SocialNetworkButtons />
           </section>
           {selectLanguageInput("select-pc-screen")}
           {toggleDarkModeButton("select-pc-screen")}
